04c43c645e3c43f5f46773425fcd0ab71bb4e853
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2025-02-16 23:03:49 +0530

    Some changes in jobs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,8 @@
 import React from "react";
-import {
-  FaFacebook,
-  FaTwitter,
-  FaLinkedin,
-  FaInstagram,
-  FaGithub,
-} from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    // <footer
-    //   className="mt-5 bg-light p-3 text-center w-100"
-    //   style={{ position: "relative", bottom: 0 }}
-    // >
-    //   <div className="container">
-    //     <h4 className="text-primary">Job Portal</h4>
-    //     <p>
-    //       <strong>Email:</strong> support@jobportal.com |{" "}
-    //       <strong>Phone:</strong> +91 9876543210
-    //     </p>
-    //     <p>
-    //       <strong>Location:</strong> Bhubaneswar, India
-    //     </p>
-
-    //     <p className="mt-3">&copy; 2025 Job Portal. All rights reserved.</p>
-    //   </div>
-    // </footer>
-
     <footer
       className="mt-5 bg-light p-3 text-center w-100"
       style={{ position: "relative", bottom: 0 }}
